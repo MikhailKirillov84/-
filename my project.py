0d57d99b6115927ed196a357a88f44f215660478
MikhailKirillov84 <monomah.car@gmail.com>
2024-09-08 11:55:26 +0300

    Add files via upload

---
+++ my project.py
@@ -0,0 +1,5 @@
+print("Hello World")
+"1st programa", print (9 ** 0.5 * 5)
+"2nd program", print(9.99 > 9.98 and 1000 != 1000.1)
+"3rd program", print(2 * 2 + 2 == 2 * (2 + 2))
+"4th program", print('123.456', '123.456' (int123.456))
